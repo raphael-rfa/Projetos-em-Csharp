6d7739755f1eb9691e58277f846dff25a00fbf78
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-11-11 19:06:14 -0300

    Biblioteca

---
+++ Biblioteca/Biblioteca/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var livros = new List<Livro>();
+            var ordenarlivros = new List<Livro>() { new Livro() { Id = 0, Titulo = " ", Autor = " ", Area = " ", Ano = 0, Editora = " " } };
+            int opcao;
+
+            do
+            {
+                Console.WriteLine("- 1 - Cadastrar \n- 2 - Imprimir Livros Cadastrados\n- 3 - Pesquisar livros por nome\n- 4 - Ordenar os livros por ano\n- 5 - Sair\n");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("\n Cadastrar livro nessa sequencia : \n Condigo: \n Titulo: \n Autor: \n Area: \n Ano: \n Editora: \n");
+                        livros.Add(new Livro() { Id = Convert.ToInt32(Console.ReadLine()), Titulo = Console.ReadLine(), Autor = Console.ReadLine(), Area = Console.ReadLine(), Ano = Convert.ToInt32(Console.ReadLine()), Editora = Console.ReadLine() });
+                        Console.WriteLine("Livro cadastrado !!!");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        foreach (var livro in livros)
+                        {
+                            Console.WriteLine("lista de todos os livros cadastrados :");
+                            Console.WriteLine($" Codigo = {livro.Id}\n titulo = {livro.Titulo}\n Autor = {livro.Autor}\n Area = {livro.Area}\n Ano = {livro.Ano}\n Editora = {livro.Editora}\n =========");
+                        }
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o livro que procura ");
+                        string plvpesquisada = Console.ReadLine();
+
+                        var pesquisa = from info in livros
+                                       where info.Titulo.Contains(plvpesquisada)
+                                       select info;
+
+                        foreach (var info in pesquisa)
+                        {
+                            Console.WriteLine($"Titulo : {info.Titulo}, Codigo : {info.Id} ");
+                        }
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        for (int i = 0; i < livros.Count - 1; i++)
+                        {
+                            for (int j = 1; j < livros.Count; j++)
+                            {
+                                if (livros[i].Ano > livros[j].Ano)
+                                {
+                                    ordenarlivros[0] = livros[i];
+                                    livros[i] = livros[j];
+                                    livros[j] = ordenarlivros[0];
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("Ordenado com sucesso !! para ver a lista inteira digite a segunda opção do menu.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    default: break;
+                }
+
+
+            } while (opcao != 5 );
+
+            Console.WriteLine("Raphael-rfa a sua disposição !!");
+        }
+        public class Livro
+        {
+            public int Id { get; set; }
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public string Area { get; set; }
+            public int Ano { get; set; }
+            public string Editora { get; set; }
+        }
+
+
+    }
+}
--- ControleDeEstoque/ControleDeEstoque/Program.cs
+++ ControleDeEstoque/ControleDeEstoque/Program.cs
@@ -4,7 +4,7 @@ namespace ControleDeEstoque
 {
     class Program
     {
-        struct Produtos
+        struct Produtos //Dados dos produtos
         {
             public string nome;
             public int qntProduzida;
@@ -13,17 +13,19 @@ namespace ControleDeEstoque
         }
 
         static void Main(string[] args)
-        {
-            int opcao;            
+        {                        
+            /* fiz esse projeto com array por isso preciso perguntar quantos 
+             * produtos tem já que o array não são autoincremete como list 
+             * mas amanha quando eu fizer um controle de livraria farei com list*/
 
             Console.WriteLine("Quantos produtos a empresa tem ?");
             int qntProdutos = Convert.ToInt32(Console.ReadLine());
 
+            
+            Produtos[] produtos = new Produtos[qntProdutos];//Array de struct
+            Produtos produtostroca = new Produtos();//Array de troca para fazer a ordenação.
 
-            Produtos[] produtos = new Produtos[qntProdutos];
-            Produtos produtostroca = new Produtos();
-
-            for (int i = 0; i < produtos.Length; i++)
+            for (int i = 0; i < produtos.Length; i++)//entrada dos dados
             {
                 Console.WriteLine($"Coloque o nome do {i+1}º produto");
                 produtos[i].nome = Console.ReadLine();
@@ -42,24 +44,21 @@ namespace ControleDeEstoque
                 for(int j = 1; j < produtos.Length; j++)
                 {
 
-                    if (produtos[i].qntEstoque > produtos[j].qntEstoque)
+                    if (produtos[i].qntEstoque > produtos[j].qntEstoque)//Ordenação em ordem crescente
                     {
                         produtostroca = produtos[i];;
-
                         produtos[i] = produtos[j];
-
                         produtos[j] = produtostroca;
-
                     }
-
                 }
             }
 
+            int opcao;
             Console.WriteLine(" 1 - Imprimir o produto com maior quantidade no estoque \n " +
                 "2 - Imprimir o produto com menor quantidade no estoque \n " +
                 "3 - imprimir o produto com maior e menor quantidade \n " +
                 "4 - Imprimir todas os produtos" +
-                "5 - Sair");
+                "5 - Sair");//Menu
 
             do
             {
@@ -114,7 +113,7 @@ namespace ControleDeEstoque
                     "4 - Imprimir todas os produtos \n " +
                     "5 - Sair");
 
-            } while (opcao != 5);
+            } while (opcao != 5);//iteração do menu
             Console.ReadLine(); 
 
         }
